45b3a3bd23ab4d9e3a2bafd45af294045b8ca632
shawal-mbalire <mbalireshawal@gmail.com>
2024-07-26 16:47:07 +0300

    eighty percent finished

--- 1strafieldSimulation/main.py
+++ 1strafieldSimulation/main.py
@@ -1,28 +1,34 @@
 import pygame
-from star import Star
+from star import Star, map_range
 
 pygame.init()
 
 width,height,length = 400,400,400
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode(
+    size = (width,height),
+)
 clock = pygame.time.Clock()
 running = True
 
 stars:list[Star] = list()
-stars_length = 100
+stars_length = 400
 
 for i in range(stars_length):
     stars.append(Star())
 
 def draw():
 
-    screen.fill('purple')
+    screen.fill('black')
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    speed = map_range(mouse_x,0,width,5,15)
+
     for star in stars:
-        star.update()
         star.show(screen)
+        star.update(speed)
 
 if __name__ == '__main__':
-    while True: 
+    while True:
         draw()
         pygame.display.flip()
+        clock.tick(20)
--- 1strafieldSimulation/star.py
+++ 1strafieldSimulation/star.py
@@ -2,18 +2,31 @@ import random
 import pygame
 
 width, height,length = 400, 400, 400
-class Star:
-    def __init__(self):
-        self.x:float = random.randint(0, width)
-        self.y:float = random.randint(0,height)
-        self.z:float = random.randint(0,length)
 
-    def update(self):
-        pass
+class Star:
+
+    def __init__(self):
+        self.x:float = random.randint(-width, width)
+        self.y:float = random.randint(-height,height)
+        self.z:float = random.randint(2,length)
+        # self.z = length
+
+    def update(self, speed):
+        self.z = self.z - speed
+        if self.z<1:
+            self.z = length
+
     def show(self, screen):
+
+        sx:float = map_range(self.x/self.z,-1,1,0,width)
+        sy:float = map_range(self.y/self.z,-1,1,0,height)
+
+        visibility = map_range(self.z,0,length,5,0)
         pygame.draw.circle(
             screen,
-            'red',
-            pygame.Vector2(self.x,self.y),
-            10
+            'green',
+            pygame.Vector2(sx,sy),
+            visibility
         )
+def map_range(x, in_min, in_max, out_min, out_max):
+    return (x - in_min)*(out_max - out_min)//(in_max - in_min) + out_min
